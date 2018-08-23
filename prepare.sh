58ce60e3e5bd43cb2ce98a13ea23b9a5e50958ca
sderunih <s.derunih@sprinthost.ru>
2018-08-23 16:15:34 +0300

    work with consul

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cat Vagrantfile  | grep '=>' |awk -F '\"' '{print $4, $2".microhosting.local"}' > hosts.file
+cat Vagrantfile |grep -v '\#' | grep '=>' |awk -F '\"' '{print $4, $2".microhosting.local"}' > hosts.file
 cat hosts.file >> /etc/hosts
-cat hosts.file |awk '{print $1}' >> hosts.setup
+cat hosts.file |awk '{print $1}' > hosts.setup
 ansible-playbook -i hosts.setup  roles/setup/tasks/main.yml  -c paramiko --ask-pass --sudo -e 'ansible_python_interpreter=/usr/bin/python3'
 
