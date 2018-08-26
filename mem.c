03213286bf7b2da377f229c4965fa9feee4236f7
sderunih <s.derunih@sprinthost.ru>
2018-08-26 07:20:41 +0300

    add some plugins and memory load tool

---
+++ mem.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    int max = 120; // сколько МБ занимать
+    int wait = 60 ; // сколько времени ждать
+    int mb = 0 ;
+    char* buffer;
+
+    while((buffer=malloc(1024*1024)) != NULL && mb != max) {
+        memset(buffer, 0, 1024*1024);
+        mb++;
+    }
+    printf("Allocated %d MB\n", mb);
+
+    while ( wait > 0 ) {
+        printf("\rHold memory for %d seconds", wait);
+        fflush( stdout );
+        sleep(1);
+        wait-- ;
+    }
+    printf("\nMemory is freed\n");
+    return 0;
+}
+
